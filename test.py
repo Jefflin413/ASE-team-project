0274b26958a98a83bf048b1c8f661279f55cdaf0
ShaoChiWu <wsc861029@gmail.com>
2020-12-07 13:24:11 -0500

    merge test_db.py and test_user.py to test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-"""unit test for utils.db"""
+"""unit test for utils.db and utils.user"""
 import unittest
 from unittest import TestCase
 
@@ -6,6 +6,7 @@ from flask import Flask
 
 import utils.db
 import utils.user
+from utils.user import User
 import os
 from datetime import date, datetime
 
@@ -449,6 +450,75 @@ class TestDb(TestCase):
             conn.execute("DELETE from advertise")
             res = utils.db.get_advertise(None)
             self.assertEqual([], res)
+
+    def test_create_user(self):
+        app = Flask(__name__)
+        with app.app_context():
+            # print("test create")
+            cur = utils.db.get_db()
+            cur.execute("DELETE FROM user")
+            id_ = "sw3525"
+            name = "Carbon"
+            email = "carbonhaha@gmail.com"
+            profile_pic = "123.png"
+            usertype = "Personal"
+            User.create(id_, name, email, profile_pic, usertype)
+            user = cur.execute("SELECT * FROM `user`")
+            rows = user.fetchall()
+            self.assertEqual(1, len(rows))
+            self.assertEqual("sw3525", rows[0]['id'])
+            self.assertEqual("Carbon", rows[0]['name'])
+            self.assertEqual("carbonhaha@gmail.com", rows[0]['email'])
+            self.assertEqual("123.png", rows[0]['profile_pic'])
+            self.assertEqual("Personal", rows[0]['usertype'])
+
+    def test_create_user_miss(self):
+        app = Flask(__name__)
+        with app.app_context():
+            # print("test create")
+            cur = utils.db.get_db()
+            cur.execute("DELETE FROM user")
+            id_ = "kkking"
+            name = None
+            email = "carbonhaha.com"
+            profile_pic = "123.png"
+            usertype = "Personal"
+            User.create(id_, name, email, profile_pic, usertype)
+            user = cur.execute("SELECT * FROM user where `id` = 'kkking'")
+            rows = user.fetchall()
+            # print(rows)
+            self.assertEqual([], rows)
+
+    def test_get_user(self):
+        app = Flask(__name__)
+        with app.app_context():
+            id_ = "sw3525"
+            name = "Carbon"
+            email = "carbonhaha@gmail.com"
+            profile_pic = "123.png"
+            usertype = "Personal"
+            User.create(id_, name, email, profile_pic, usertype)
+            user = User.get("sw3525")
+            self.assertEqual("sw3525", user.id)
+            self.assertEqual("Carbon", user.name)
+            self.assertEqual("carbonhaha@gmail.com", user.email)
+            self.assertEqual("123.png", user.profile_pic)
+            self.assertEqual("Personal", user.usertype)
+
+            # print("Ahoy!")
+
+    def test_get_user_miss(self):
+        app = Flask(__name__)
+        with app.app_context():
+            id_ = "sw3525"
+            name = "Carbon"
+            email = "carbonhaha@gmail.com"
+            profile_pic = "123.png"
+            usertype = "Personal"
+            User.create(id_, name, email, profile_pic, usertype)
+            user = User.get("sw9999")
+            self.assertEqual(None, user)
+            # print("Ahoy!")
 
     def tearDown(self):
         os.system("git restore utils/sqlite_db")
